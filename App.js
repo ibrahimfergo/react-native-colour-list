aafbf10e90c2ed97f2f60238852fbf6a6d3574a4
Ibrahim fergo <ibrahimfergo@gmail.com>
2021-01-05 01:47:26 +0800

    Added props for BlockRGB components

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import BlockRGB from "./components/BlockRGB";
+
 
 function HomeScreen() {
   return (
@@ -11,8 +13,8 @@ function HomeScreen() {
       <BlockRGB red={0} green={255} blue={0} />
       <BlockRGB red={0} green={0} blue={255} />
     </View>
-  );
- 
+ );
+}
 
 const Stack = createStackNavigator();
 
