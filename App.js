2bff7cf6cf0a91602933e49c30c910fe08f3f3b7
Ibrahim fergo <ibrahimfergo@gmail.com>
2021-01-05 02:31:06 +0800

    Change colurs to state variable

--- App.js
+++ App.js
@@ -6,12 +6,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 import { FlatList } from "react-native-gesture-handler";
 
-const COLOURS = [
-  {red: 255, green: 128, blue: 0, id: "0"},
-  {red: 0, green: 128, blue: 255, id: "1"},
-  {red: 128, green: 0, blue: 255, id: "2"},
-]
+// const COLOURS = [
+//  {red: 255, green: 128, blue: 0, id: "0"},
+//  {red: 0, green: 128, blue: 255, id: "1"},
+//  {red: 128, green: 0, blue: 255, id: "2"},
+// ];
+
 function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+  { red: 255, green: 128, blue: 128, id: "0" },
+  { red: 0, green: 128, blue: 255, id: "1" },
+  { red: 128, green: 0, blue: 255, id: "2" },
+]);
+
  function renderItem({ item }) {
 return <BlockRGB red={item.red} green={item.green}blue={item.blue} />;
 }
@@ -29,7 +36,7 @@ export default function App() {
  return (
    <NavigationContainer>
      <Stack.Navigator>
-       <Stack.Screen name="Home" component={HomeScreen} />
+       <Stack.Screen name="Kueh Lapis" component={HomeScreen} />
      </Stack.Navigator>
    </NavigationContainer>
  );
